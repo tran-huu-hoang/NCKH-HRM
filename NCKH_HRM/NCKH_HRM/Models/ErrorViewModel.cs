6832c88f89b0aefe86b7d7f6dcf9cd363921aed9
unknown <tranhuuhoang1407@gmail.com>
2024-05-08 11:32:43 +0700

    day01: create project & layout

---
+++ NCKH_HRM/NCKH_HRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NCKH_HRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
