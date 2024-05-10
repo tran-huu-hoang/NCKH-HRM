9ecc3f373ce084b7384fddab30dca849f45706bc
unknown <tranhuuhoang1407@gmail.com>
2024-05-10 14:52:33 +0700

    Day03: feat: CURD/Paging/Search Classes, Session

---
+++ NCKH_HRM/NCKH_HRM/Areas/Admin/Controllers/SessionsController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NCKH_HRM.Models;
+using Newtonsoft.Json;
+using X.PagedList;
+
+namespace NCKH_HRM.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SessionsController : BaseController
+    {
+        private readonly NckhDbContext _context;
+
+        public SessionsController(NckhDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Sessions
+        public async Task<IActionResult> Index(string name, int page = 1)
+        {
+            //số bản ghi trên 1 trang
+            int limit = 5;
+
+            var session = await _context.Sessions.OrderBy(c => c.Id).ToPagedListAsync(page, limit);
+            if (!String.IsNullOrEmpty(name))
+            {
+                session = await _context.Sessions.Where(c => c.Name.Contains(name)).OrderBy(c => c.Id).ToPagedListAsync(page, limit);
+            }
+            ViewBag.keyword = name;
+            return View(session);
+
+        }
+
+        // GET: Admin/Sessions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Sessions == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // GET: Admin/Sessions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Sessions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,StartDate,CreateBy,UpdateBy,CreateDate,UpdateDate,IsDelete,IsActive")] Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = JsonConvert.DeserializeObject<UserStaff>(HttpContext.Session.GetString("AdminLogin"));
+                session.CreateBy = user.Username;
+                session.UpdateBy = user.Username;
+                session.IsDelete = false;
+                _context.Add(session);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(session);
+        }
+
+        // GET: Admin/Sessions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Sessions == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            return View(session);
+        }
+
+        // POST: Admin/Sessions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,StartDate,CreateBy,UpdateBy,CreateDate,UpdateDate,IsDelete,IsActive")] Session session)
+        {
+            if (id != session.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = JsonConvert.DeserializeObject<UserStaff>(HttpContext.Session.GetString("AdminLogin"));
+                    session.UpdateBy = user.Username;
+                    session.UpdateDate = DateTime.Now;
+                    _context.Update(session);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SessionExists(session.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(session);
+        }
+
+        // GET: Admin/Sessions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            /*if (id == null || _context.Sessions == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);*/
+            if (_context.Sessions == null)
+            {
+                return Problem("Entity set 'NckhDbContext.Sessions'  is null.");
+            }
+            var session = await _context.Sessions.FindAsync(id);
+            if (session != null)
+            {
+                _context.Sessions.Remove(session);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Admin/Sessions/Delete/5
+        /*[HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Sessions == null)
+            {
+                return Problem("Entity set 'NckhDbContext.Sessions'  is null.");
+            }
+            var session = await _context.Sessions.FindAsync(id);
+            if (session != null)
+            {
+                _context.Sessions.Remove(session);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }*/
+
+        private bool SessionExists(int id)
+        {
+          return (_context.Sessions?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
