6fc7578a20b88849f83f6bdd8c9a24ad04194ad0
unknown <mhung4011@gmail.com>
2024-05-11 11:37:46 +0700

    Day04: feat: CURD/Paging/Search table PointSys4, Major

---
+++ NCKH_HRM/NCKH_HRM/Areas/Admin/Controllers/MajorsController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NCKH_HRM.Models;
+using Newtonsoft.Json;
+using X.PagedList;
+
+namespace NCKH_HRM.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MajorsController : BaseController
+    {
+        private readonly NckhDbContext _context;
+
+        public MajorsController(NckhDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Majors
+        public async Task<IActionResult> Index(string name, int page = 1)
+        {
+            int limit = 5;
+
+            var major = await _context.Majors.OrderBy(c => c.Id).ToPagedListAsync(page, limit);
+            if (!String.IsNullOrEmpty(name))
+            {
+                major = await _context.Majors.Where(c => c.Name.Contains(name)).OrderBy(c => c.Id).ToPagedListAsync(page, limit);
+            }
+            ViewBag.keyword = name;
+            return View(major);
+        }
+
+        // GET: Admin/Majors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Majors == null)
+            {
+                return NotFound();
+            }
+
+            var major = await _context.Majors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (major == null)
+            {
+                return NotFound();
+            }
+
+            return View(major);
+        }
+
+        // GET: Admin/Majors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Majors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,CreateBy,UpdateBy,CreateDate,UpdateDate,IsDelete,IsActive")] Major major)
+        {
+            if (ModelState.IsValid)
+            {
+                var admin = JsonConvert.DeserializeObject<UserStaff>(HttpContext.Session.GetString("AdminLogin"));
+                major.CreateBy = admin.Username;
+                major.UpdateBy = admin.Username;
+                major.IsDelete = false;
+
+                _context.Add(major);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(major);
+        }
+
+        // GET: Admin/Majors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Majors == null)
+            {
+                return NotFound();
+            }
+
+            var major = await _context.Majors.FindAsync(id);
+            if (major == null)
+            {
+                return NotFound();
+            }
+            return View(major);
+        }
+
+        // POST: Admin/Majors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,CreateBy,UpdateBy,CreateDate,UpdateDate,IsDelete,IsActive")] Major major)
+        {
+            if (id != major.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = JsonConvert.DeserializeObject<UserStaff>(HttpContext.Session.GetString("AdminLogin"));
+                    major.UpdateBy = user.Username;
+                    major.UpdateDate = DateTime.Now;
+
+                    _context.Update(major);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MajorExists(major.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(major);
+        }
+
+        // GET: Admin/Majors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            /*if (id == null || _context.Majors == null)
+            {
+                return NotFound();
+            }
+
+            var major = await _context.Majors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (major == null)
+            {
+                return NotFound();
+            }
+
+            return View(major);*/
+            if (_context.Majors == null)
+            {
+                return Problem("Entity set 'NckhDbContext.Majors'  is null.");
+            }
+            var major = await _context.Majors.FindAsync(id);
+            if (major != null)
+            {
+                _context.Majors.Remove(major);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Admin/Majors/Delete/5
+        /*[HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Majors == null)
+            {
+                return Problem("Entity set 'NckhDbContext.Majors'  is null.");
+            }
+            var major = await _context.Majors.FindAsync(id);
+            if (major != null)
+            {
+                _context.Majors.Remove(major);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }*/
+
+        private bool MajorExists(int id)
+        {
+          return (_context.Majors?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
