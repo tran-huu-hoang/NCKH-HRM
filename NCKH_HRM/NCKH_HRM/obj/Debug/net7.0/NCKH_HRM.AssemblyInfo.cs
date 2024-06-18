290560877bafa04d53579a85a064b3e742d9504c
unknown <mhung4011@gmail.com>
2024-06-18 20:47:14 +0700

    Day37: fix: update some admin front-end

--- NCKH_HRM/NCKH_HRM/obj/Debug/net7.0/NCKH_HRM.AssemblyInfo.cs
+++ NCKH_HRM/NCKH_HRM/obj/Debug/net7.0/NCKH_HRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NCKH_HRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ceb07236176f8f03cc851fafbbb55cd847228f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NCKH_HRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NCKH_HRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
