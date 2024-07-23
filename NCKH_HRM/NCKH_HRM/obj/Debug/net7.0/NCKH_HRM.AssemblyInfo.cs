79d1d7a239e5a3d728e20f48e36455b397a04d3c
unknown <tranhuuhoang1407@gmail.com>
2024-07-23 19:29:42 +0700

    Day 63: update: Score detail for student front-end

--- NCKH_HRM/NCKH_HRM/obj/Debug/net7.0/NCKH_HRM.AssemblyInfo.cs
+++ NCKH_HRM/NCKH_HRM/obj/Debug/net7.0/NCKH_HRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NCKH_HRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fe2f57199b99e238cc309a8c906398666b65ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469131d3579b83a60c034e4879908f6829c4f757")]
 [assembly: System.Reflection.AssemblyProductAttribute("NCKH_HRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NCKH_HRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
